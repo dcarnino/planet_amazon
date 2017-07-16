f510ad73d4bf27509720d9df0c1bb2ac54669e0e
dcarnino <deniscarnino@gmail.com>
2017-07-16 17:29:32 +0200

    add: test function for inceptionv3

--- test_model_inceptionv3.py
+++ test_model_inceptionv3.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         with open("../data/planet_amazon/inceptionv3_preds%d.npy"%fold_id, "rb") as iOF:
             y_pred_fold = np.load(iOF)
 
-        y_pred_fold = np.median(y_pred_fold, axis=0)
+        y_pred_fold = np.mean(y_pred_fold, axis=0)
         y_pred.append(y_pred_fold)
 
         y_true_fold = df_val.iloc[:,2:].values
@@ -44,8 +44,8 @@ if __name__ == '__main__':
     y_pred = np.vstack(y_pred)
     y_true = np.vstack(y_true)
 
-    y_pred[y_pred > 0.5] = 1
-    y_pred[y_pred < 0.5] = 0
+    y_pred[y_pred > 0.2] = 1
+    y_pred[y_pred < 0.2] = 0
 
     y_pred = y_pred.astype(int)
 
