0ee8642f973e2f0dae5c70251864c05e0aa8aa07
dcarnino <deniscarnino@gmail.com>
2017-07-16 18:33:19 +0200

    add: test function for inceptionv3

--- test_model_inceptionv3.py
+++ test_model_inceptionv3.py
@@ -46,8 +46,8 @@ if __name__ == '__main__':
     y_pred = np.vstack(y_pred)
     y_true = np.vstack(y_true)
 
-    y_pred[y_pred > 0.2] = 1
-    y_pred[y_pred < 0.2] = 0
+    y_pred[y_pred > 0.5] = 1
+    y_pred[y_pred < 0.5] = 0
 
     y_pred = y_pred.astype(int)
 
