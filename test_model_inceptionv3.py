ebd8334aa7639c45d38b60839f3c85484a34538e
dcarnino <deniscarnino@gmail.com>
2017-07-17 13:02:06 +0200

    add: xgb

--- test_model_inceptionv3.py
+++ test_model_inceptionv3.py
@@ -142,9 +142,9 @@ if __name__ == '__main__':
                     yy_train, yy_test = y_true_feat[train_index], y_true_feat[test_index]
 
                     clf = XGBClassifier_ensembling(n_folds=5, early_stopping_rounds=10,
-                                                   max_depth=10, learning_rate=0.02,
+                                                   max_depth=5, learning_rate=0.02,
                                                    objective='binary:logistic', nthread=28,
-                                                   min_child_weight=4, subsample=0.8)
+                                                   min_child_weight=4, subsample=0.7)
 
                     clf.fit(XX_train, yy_train)
 
@@ -171,8 +171,8 @@ if __name__ == '__main__':
 
         if cross_validate:
 
-            #f2_threshs = optimise_f2_thresholds(y_true, y_pred_xgb, resolution=100)
-            f2_threshs = [0.5]*17
+            f2_threshs = optimise_f2_thresholds(y_true, y_pred_xgb, resolution=100)
+            #f2_threshs = [0.5]*17
 
             with open("../data/planet_amazon/optimized_thresholds_xgb.txt", "w") as iOF:
                 iOF.writelines([str(thresh)+"\n" for thresh in f2_threshs])
