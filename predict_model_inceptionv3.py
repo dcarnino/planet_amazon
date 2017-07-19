d296c3d999efcb440fb1bef22f4feae97498f5c4
dcarnino <deniscarnino@gmail.com>
2017-07-19 17:15:49 +0200

    add: n_inference set to 20

--- predict_model_inceptionv3.py
+++ predict_model_inceptionv3.py
@@ -160,7 +160,7 @@ def main(verbose=1):
 
         ### Infer
         if verbose >= 1: print("Inferring (fold %d)..."%fold_id)
-        n_inference = 50
+        n_inference = 20
         y_pred, y_test = infer(model, X_val, y_val, n_inference=n_inference, batch_size=32, verbose=verbose)
         if verbose >= 2:
             print(y_pred.shape)
@@ -218,7 +218,7 @@ def main(verbose=1):
 
             ### Infer
             if verbose >= 1: print("Inferring (fold %d)..."%fold_id)
-            n_inference = 50
+            n_inference = 20
             y_pred, _ = infer(model, X_test, y_test, n_inference=n_inference//5, batch_size=32, verbose=verbose)
             y_pred_folds.append(y_pred)
             if verbose >= 2:
