23a84d2e6315035c0e95ae51d948a93e5b5a9a25
dcarnino <deniscarnino@gmail.com>
2017-07-20 23:28:46 +0200

    add: logit params

--- test_models.py
+++ test_models.py
@@ -66,11 +66,8 @@ def main_val():
     net_list = ["inceptionv3", "vgg16", "resnet50", "inceptionv3_128", "vgg16_128", "resnet50_128", "xgboost", "extratrees"]
     offset_list = [0, 5, 10, 15, 20, 25, 90, 100]
     n_folds_list = [5, 5, 5, 5, 5, 5, 10, 10]
-    net_list[3:4] = []
     net_list[5:6] = []
-    offset_list[3:4] = []
     offset_list[5:6] = []
-    n_folds_list[3:4] = []
     n_folds_list[5:6] = []
     for net, offset, n_folds in zip(net_list, offset_list, n_folds_list):
 
