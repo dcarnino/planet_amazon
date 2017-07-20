77a0a25d322e0f0bc4535526378e632f04c160dc
dcarnino <deniscarnino@gmail.com>
2017-07-20 19:17:25 +0200

    add: logit params

--- test_models.py
+++ test_models.py
@@ -54,7 +54,7 @@ def optimise_f2_thresholds(y, p, resolution=100, bmin=0., bmax=1., verbose=1):
 def main_val():
 
     mean_only = True
-    cross_validate = False
+    cross_validate = True
 
     print("Importing features...")
     df_feat = pd.read_csv("../data/planet_amazon/train_features.csv").rename(columns={"0": "image_name"})
@@ -267,60 +267,85 @@ def main_val():
 
 def main_test():
 
-    mean_only = False
+    mean_only = True
 
     y_true = []
     y_pred_mean, y_pred_median, y_pred_std, y_pred_min, y_pred_max, y_pred_skew, y_pred_kurtosis, y_pred_iqr, y_pred_entropy = [], [], [], [], [], [], [], [], []
 
-    with open("../data/planet_amazon/inceptionv3_idstest.txt", "r") as iOF:
-        test_ids = iOF.readlines()
-    test_ids = [tid[:-1] for tid in test_ids]
+    net_preds = {}
+    net_image_ids = {}
+    net_list = ["inceptionv3", "vgg16", "resnet50", "inceptionv3_128", "vgg16_128", "resnet50_128", "xgboost", "extratrees"]
+    offset_list = [0, 5, 10, 15, 20, 25, 90, 100]
+    n_folds_list = [5, 5, 5, 5, 5, 5, 10, 10]
+    net_list[3:6] = []
+    offset_list[3:6] = []
+    n_folds_list[3:6] = []
 
-    with open("../data/planet_amazon/inceptionv3_predstest.npy", "rb") as iOF:
-        y_pred_fold = np.load(iOF)
+    for net, offset, n_folds in zip(net_list, offset_list, n_folds_list):
 
-    y_pred_fold_mean = np.mean(y_pred_fold, axis=0)
-    y_pred_fold_median = np.median(y_pred_fold, axis=0)
-    y_pred_fold_std = np.std(y_pred_fold, axis=0)
-    y_pred_fold_min = np.min(y_pred_fold, axis=0)
-    y_pred_fold_max = np.max(y_pred_fold, axis=0)
-    y_pred_fold_skew = skew(y_pred_fold, axis=0, nan_policy='omit')
-    y_pred_fold_skew[~np.isfinite(y_pred_fold_skew)] = 0.
-    y_pred_fold_kurtosis = kurtosis(y_pred_fold, axis=0, nan_policy='omit')
-    y_pred_fold_kurtosis[~np.isfinite(y_pred_fold_kurtosis)] = 0.
-    y_pred_fold_iqr = iqr(y_pred_fold, axis=0)
-    y_pred_fold_entropy = entropy(y_pred_fold)
-    y_pred_mean.append(y_pred_fold_mean)
-    y_pred_median.append(y_pred_fold_median)
-    y_pred_std.append(y_pred_fold_std)
-    y_pred_min.append(y_pred_fold_min)
-    y_pred_max.append(y_pred_fold_max)
-    y_pred_skew.append(y_pred_fold_skew)
-    y_pred_kurtosis.append(y_pred_fold_kurtosis)
-    y_pred_iqr.append(y_pred_fold_iqr)
-    y_pred_entropy.append(y_pred_fold_entropy)
+        print("Processing model %s..."%net)
 
-    y_pred_mean = np.vstack(y_pred_mean)
-    y_pred_median = np.vstack(y_pred_median)
-    y_pred_std = np.vstack(y_pred_std)
-    y_pred_min = np.vstack(y_pred_min)
-    y_pred_max = np.vstack(y_pred_max)
-    y_pred_skew = np.vstack(y_pred_skew)
-    y_pred_kurtosis = np.vstack(y_pred_kurtosis)
-    y_pred_iqr = np.vstack(y_pred_iqr)
-    y_pred_entropy = np.vstack(y_pred_entropy)
+        if n_folds == 5:
+            with open("../data/planet_amazon/%s_idstest_20.txt"%net, "r") as iOF:
+                test_ids = iOF.readlines()
+            test_ids = [tid[:-1] for tid in test_ids]
 
-    y_pred = np.array([y_pred_mean, y_pred_median, y_pred_std, y_pred_min, y_pred_max, y_pred_skew, y_pred_kurtosis, y_pred_iqr, y_pred_entropy])
+            y_pred_fold = np.load("../data/planet_amazon/%s_predstest_20.npy"%net)
+        else:
+            with open("../data/planet_amazon/%s_idstest.txt"%net, "r") as iOF:
+                test_ids = iOF.readlines()
+            test_ids = [tid[:-1] for tid in test_ids]
+
+            y_pred_fold = np.load("../data/planet_amazon/%s_predstest.npy"%net)
+
+        y_pred_fold_mean = np.mean(y_pred_fold, axis=0)
+        y_pred_fold_median = np.median(y_pred_fold, axis=0)
+        y_pred_fold_std = np.std(y_pred_fold, axis=0)
+        y_pred_fold_min = np.min(y_pred_fold, axis=0)
+        y_pred_fold_max = np.max(y_pred_fold, axis=0)
+        y_pred_fold_skew = skew(y_pred_fold, axis=0, nan_policy='omit')
+        y_pred_fold_skew[~np.isfinite(y_pred_fold_skew)] = 0.
+        y_pred_fold_kurtosis = kurtosis(y_pred_fold, axis=0, nan_policy='omit')
+        y_pred_fold_kurtosis[~np.isfinite(y_pred_fold_kurtosis)] = 0.
+        y_pred_fold_iqr = iqr(y_pred_fold, axis=0)
+        y_pred_fold_entropy = entropy(y_pred_fold)
+
+        y_pred = np.array([y_pred_mean, y_pred_median, y_pred_std, y_pred_min, y_pred_max, y_pred_skew, y_pred_kurtosis, y_pred_iqr, y_pred_entropy
+
+        net_preds[net] = y_pred
+        net_image_ids[net] = np.array(test_ids)
+
+        ### assert order is correct
+        for net_name in net_list[1:]:
+            np.testing.assert_array_equal(net_image_ids[net_list[0]], net_image_ids[net_name], err_msg="Conflicting values for nets %s and %s."%(net_list[0], net_name))
 
     if mean_only:
 
-        with open("../data/planet_amazon/optimized_thresholds_inceptionv3.txt", "r") as iOF:
+        y_pred = np.array([net_preds[net_name][0,:,:] for net_name in net_list])
+
+        y_pred_logit = np.zeros_like(net_preds[net_list[0]][0,:,:], dtype=np.float)
+
+        for ix_feat in range(17):
+
+            print("Logit feat %d/%d..."%(ix_feat+1,17))
+
+            with gzip.open("../data/planet_amazon/models/logit_class%d.gzip"%ix_feat, "rb") as iOF:
+                clf = pickle.load(iOF)
+
+            y_pred_feat = y_pred[..., ix_feat].T
+            y_true_feat = y_true[..., ix_feat]
+
+            yy_pred = np.array([p2 for p1, p2 in clf.predict_proba(y_pred_feat)])
+
+            y_pred_logit[:, ix_feat] = yy_pred
+
+        with open("../data/planet_amazon/optimized_thresholds_logit.txt", "r") as iOF:
             f2_threshs = iOF.readlines()
         f2_threshs = [float(thresh[:-1]) for thresh in f2_threshs]
 
-        y_pred2 = np.zeros_like(y_pred_mean)
+        y_pred2 = np.zeros_like(y_pred_logit)
         for i in range(17):
-            y_pred2[:, i] = (y_pred_mean[:, i] - f2_threshs[i]).astype(np.float)
+            y_pred2[:, i] = (y_pred_logit[:, i] - f2_threshs[i]).astype(np.float)
 
     else:
 
@@ -359,6 +384,7 @@ def main_test():
     weather_mask = (np.argsort(y_pred_weather, axis=1)[-4:-1])
     y_pred2[weather_mask] = -1."""
 
+    test_ids = net_image_ids[net_list[0]]
     print(len(test_ids))
     print(y_pred2.shape)
     print(y_pred2[:3,:])
@@ -373,7 +399,7 @@ def main_test():
     pred_labels = [" ".join(labels[np.where(yp > 0.5)]) for yp in y_pred2]
 
     df = pd.DataFrame(np.array([test_ids, pred_labels]).T, columns=["image_name", "tags"])
-    df.to_csv("../data/planet_amazon/submission_file_003.csv", index=False)
+    df.to_csv("../data/planet_amazon/submission_file_004.csv", index=False)
 
 
 
@@ -384,4 +410,4 @@ def main_test():
 #                   Main
 #==============================================
 if __name__ == '__main__':
-    main_val()
+    main_test()
