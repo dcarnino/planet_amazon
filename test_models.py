fa7886a7e4132c6f22ba73729373adb9a315b41e
dcarnino <deniscarnino@gmail.com>
2017-07-20 13:04:42 +0200

    add: logit on all nets and models.

--- test_models.py
+++ test_models.py
@@ -64,7 +64,7 @@ def main_val():
     net_preds = {}
     net_trues = {}
     net_list = ["inceptionv3", "vgg16", "resnet50", "inceptionv3_128", "vgg16_128", "resnet50_128", "xgboost", "extratrees"]
-    offset_list = [0, 5, 10, 15, 20, 25, 30, 90, 100]
+    offset_list = [0, 5, 10, 15, 20, 25, 90, 100]
     n_folds_list = [5, 5, 5, 5, 5, 5, 10, 10]
     net_list[3:6] = []
     offset_list[3:6] = []
