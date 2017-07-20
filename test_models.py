dcaa5c8d8737a6e1d56733a8c7b6403a1b7f272d
dcarnino <deniscarnino@gmail.com>
2017-07-21 00:36:53 +0200

    add: logit params

--- test_models.py
+++ test_models.py
@@ -329,7 +329,8 @@ def main_test():
 
     if mean_only:
 
-        y_pred = np.array([net_preds[net_name][0,:,:] for net_name in net_list])
+        y_pred = np.array([net_preds[net_name][0,:,:] for net_name in net_list]+\
+                          [net_preds[net_name][4,:,:] for net_name in net_list if net_name != "xgboost" and net_name != "extratrees"])
 
         y_pred_logit = np.zeros_like(net_preds[net_list[0]][0,:,:], dtype=np.float)
 
